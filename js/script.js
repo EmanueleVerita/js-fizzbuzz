e3696cbbc8cd58493da7b9749d20859416a2594c
EmanueleVerita <emanuele.verita@gmail.com>
2023-06-14 16:50:33 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
 
 
-const div = document.getElementById('my-div')
 
 const ul = document.querySelector('.number-list')
+
+let text;
 
 for(let i = 1 ; i <= 100 ; i++){
 
 
-    console.log('i' , i , typeof i)
-    
+   // console.log('i' , i , typeof i)
+   if((i % 3 == 0) && (i % 5 == 0)){
+
+    console.log('fizzbuzz');
+    text = 'FizzBuzz';
+
+}else 
+    if(i % 3 == 0){
+
+        console.log('fizz');
+        text = 'Fizz';
+
+    }
+
+    else if(i % 5 == 0){
+
+        console.log('buzz');
+        text = 'Buzz';
+
+    }
+    else{
+        console.log(i)
+        text = i;
+
+    }
+
     const newLi = document.createElement('li');
-    newLi.innerHTML = i;
+    newLi.innerHTML = text;
     ul.append(newLi);
-
-
-    
 
 }
